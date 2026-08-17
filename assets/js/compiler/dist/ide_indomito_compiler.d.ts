6850b155ab85d93e974eb68116327d2903303fb4
Yahir Guevara Cardona <yahir@devuan>
2026-08-16 23:32:15 -0600

    Agregar archivos WASM del compilador que estaban ignorados

---
+++ assets/js/compiler/dist/ide_indomito_compiler.d.ts
@@ -0,0 +1,50 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function analizar_lexico(codigo: string): any;
+
+export function analizar_semantico(codigo: string): any;
+
+export function analizar_sintactico(codigo: string): any;
+
+export function generar_intermedio(codigo: string): string;
+
+export function generar_simbolos(codigo: string): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly analizar_lexico: (a: number, b: number) => any;
+    readonly analizar_semantico: (a: number, b: number) => any;
+    readonly analizar_sintactico: (a: number, b: number) => any;
+    readonly generar_intermedio: (a: number, b: number) => [number, number];
+    readonly generar_simbolos: (a: number, b: number) => any;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ assets/js/compiler/dist/ide_indomito_compiler_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const analizar_lexico: (a: number, b: number) => any;
+export const analizar_semantico: (a: number, b: number) => any;
+export const analizar_sintactico: (a: number, b: number) => any;
+export const generar_intermedio: (a: number, b: number) => [number, number];
+export const generar_simbolos: (a: number, b: number) => any;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
